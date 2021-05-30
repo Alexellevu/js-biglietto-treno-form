0018fbb9fe038019b8a61d4bcef43e6d1eba0f22
alexellevu <newnapster83@gmail.com>
2021-05-30 19:37:41 +0200

    set prezzi in js

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,36 @@
 
-document.getElementById('genera_ticket').addEventListener('click',generaTicket);
+document.getElementById('genera_ticket_click').addEventListener('click',generaTicket);
 
     function generaTicket(event){
     event.preventDefault();
-    var nomeEl = document.getElementById('nome').value;
+    var nomeEl = document.getElementById('nome');
     console.log(nomeEl);
-    var kmEl = document.getElementById('km').value;
+    var kmEl = document.getElementById('km');
     console.log(kmEl);
-    var gruppiEtà = document.getElementById('età');
+    var gruppiEtà = document.getElementById('anni');
+    console.log(gruppiEtà);
+    var prezzoPieno = kmEl.value * 0.21;
+   
 
-    var prezzoPieno = kmEl.value *0.21;
 
     //sconto 20%minorenne
     if(gruppiEtà.value =='minorenne'){
-        var prezzoMin = prezzoPieno * 0.2;
+        prezzoPieno -= prezzoPieno * 0.2;
+        
+
+    //sconto 40% over65
     }else if(gruppiEtà.value =='over65'){
-        var prezzoOver = prezzoPieno *0.4;
+        prezzoPieno -= prezzoPieno * 0.4;
+        
+    }
+    console.log( prezzoPieno);
     }
 
-    }
 
+
+    
+
+  
     //funzione per i numeri casuali
     function randomNumber(min, max) {
         return Math.floor(Math.random() * ( max - min )) + min
